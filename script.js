e46b50cfe31e76653feeb4529549a433eee8cd04
Yordi Sanchez <132956760+ysanchezf@users.noreply.github.com>
2024-07-05 21:45:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+// Mostrar una alerta mas inforacion
 function showAlert() {
     alert('Bienvenido a Finanzas Inteligentes. Aprende a gestionar tu dinero de manera efectiva. Realizado por Yordi Sanchez; 21-0834.');
 }
-
+// Manejo del envío del formulario de contacto
 function sendEmail(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    
+    // Mostrar una alerta cuando se envíe el formulario
     alert(`Correo enviado por ${name} (${email}): ${message}`);
     
 }
-
+// Alternar el modo oscuro en la página web.
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
+// Cambiar color al pasar el mouse
+const hoverElements = document.querySelectorAll('.Efectocolor');
+hoverElements.forEach(function (element) {
+    element.addEventListener('mouseover', function () {
+        element.style.backgroundColor = 'red';
+    });
+    element.addEventListener('mouseout', function () {
+        element.style.backgroundColor = '';
+    });
+});
